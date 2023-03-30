4ab23696dea05358db441a319b9e4969a77129da
kichalomex <kichalo23con@hotmail.com>
2023-03-29 18:49:30 -0600

    Update notes.js

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'http://localhost:3001/api/notes'
+const url = 'https://notes-server-rofq.onrender.com/api/notes'
 
 const getAll = () => {
     const request = axios.get(url)
